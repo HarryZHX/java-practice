407881917573f12e6af90df6854e11007c7f0fa9
HarryZHX <harryhaoxuan@gmail.com>
2020-02-19 21:15:57 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
+        printStrg();
+        int[] ary = {1, 2, 3, 4, 5};
+        printAryIndex(ary,3);
+        handleExc(ary);
+    }
 
-        System.out.println("Hello World!");
+    public static void printStrg(){
+        for (int i = 0; i < 11; i ++){
+            if (i == 7){
+                continue;
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+    public static void printAryIndex(int[] ary, int n){
+        System.out.println(ary[n]);
+    }
+    public static void handleExc(int[] ary){
+        try{
+            int var = ary.length;
+            System.out.println(ary[var]);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
