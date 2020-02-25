de0336dcfd461a419de5e36c7a9851223453f235
HarryZHX <harryhaoxuan@gmail.com>
2020-02-24 22:32:51 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        String str = "Was it a cat I saw";
+        String str = "Was it a cat I saw!$%^^^^^";
         checkPalindrome(str);
     }
 
-
     public static void checkPalindrome(String str) {
         String allLowStr = str.toLowerCase();
-        String cleanStr = allLowStr.replaceAll("\\s+", "");
+        String cleanStr = allLowStr.replaceAll("\\W", "");
         int finalIndex = cleanStr.length() - 1;
         for (int i = 0; i < cleanStr.length(); i++) {
             if (cleanStr.charAt(i) == cleanStr.charAt(finalIndex - i)) {
